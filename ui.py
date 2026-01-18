02c4596c55302a17bc0f7a6743ad909cb15b0925
chirag <chiragkaintura@gmail.com>
2026-01-18 12:24:10 +0530

    initial expense tracker project

--- ui.py
+++ ui.py
@@ -1,1 +1,9 @@
+def show_menue():
+    print("\nExpensetracker")
+    print("1. add expense")
+    print("2. show expense")
+    print("3. show total")
+    print("4. exit")
 
+def get_user_choice():
+    return input("Choose an option: ")
